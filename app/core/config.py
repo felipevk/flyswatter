04a516f985e372069fdd7414d69a4a71a9721e7b
Pedro Kauati <pedrokauati@gmail.com>
2025-09-14 15:32:11 +0200

    Updated config and env vars

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,23 @@
 from pydantic import BaseModel
 import os
 
+def build_db_host() -> str:
+    match os.getenv("APP_ENV", ""): 
+        case "dev":
+            return os.getenv("DB_HOST_MIGRATION", "")
+        case _:
+            return os.getenv("DB_HOST", "")
+
+def build_db_url() -> str:
+    user = os.getenv("POSTGRES_USER", "")
+    pwd  = os.getenv("POSTGRES_PASSWORD", "")
+    host = build_db_host()
+    port = int(os.getenv("DB_PORT", ""))
+    db   = os.getenv("POSTGRES_DB", "")
+    driver = os.getenv("DB_DRIVER", "")
+    return f"{driver}://{user}:{pwd}@{host}:{port}/{db}"
+
 class Settings(BaseModel):
-    database_url: str = os.getenv("DATABASE_URL", "")
+    database_url: str = build_db_url()
 
 settings = Settings()
