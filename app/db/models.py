c714c528c3143f20b51de94ce378c3e491f38334
Pedro Kauati <pedrokauati@gmail.com>
2025-09-18 12:32:16 +0200

    Added refresh tokens to db

--- app/db/models.py
+++ app/db/models.py
@@ -60,6 +60,8 @@ class User(Base):
 
     memberships: Mapped[List["Membership"]] = relationship(back_populates="user", cascade="all, delete-orphan")
 
+    refresh_tokens: Mapped[List["RefreshToken"]] = relationship(back_populates="user", cascade="all, delete-orphan")
+
 
 class Project(Base):
     __tablename__ = "projects"
@@ -136,3 +138,15 @@ class Comment(Base):
 
     author: Mapped["User"] = relationship(back_populates="comments")
     issue: Mapped["Issue"] = relationship(back_populates="comments")
+
+class RefreshToken(Base):
+    __tablename__ = "refresh"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    public_id: Mapped[str] = mapped_column(String(32), unique=True,
+                                           nullable=False, index=True)
+    expires_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    revoked_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+    user: Mapped["User"] = relationship(back_populates="refresh_tokens")
