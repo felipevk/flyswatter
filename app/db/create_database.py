0cb7113c81d7b47d1849077f7e2cd8e8072c5051
Pedro Kauati <pedrokauati@gmail.com>
2025-09-20 11:52:33 +0200

    fixed main check

--- app/db/create_database.py
+++ app/db/create_database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from app.core.config import settings
 from sqlalchemy_utils import database_exists, create_database
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     engine = create_engine(settings.database_url, future=True)
     if not database_exists(engine.url):
         create_database(engine.url)
