582229c7429c61d301573f8c59df78d9b1fd8b2b
Pedro Kauati <pedrokauati@gmail.com>
2025-09-25 12:27:31 +0200

    Fixed function call order in create db

--- app/db/create_database.py
+++ app/db/create_database.py
@@ -3,12 +3,13 @@ from sqlalchemy_utils import create_database, database_exists
 
 from app.core.config import settings
 
-if __name__ == "__main__":
-    create_db()
-
 
 def create_db():
     print(f"Creating db at {settings.database_url}")
     engine = create_engine(settings.database_url, future=True)
     if not database_exists(engine.url):
         create_database(engine.url)
+
+
+if __name__ == "__main__":
+    create_db()
